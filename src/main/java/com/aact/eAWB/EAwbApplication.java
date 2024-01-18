381c053c679b7a4d5f2d23be1bca232f577da5ef
heesub <heesub3229@naver.com>
2024-01-18 17:58:36 +0900

    TEST

---
+++ src/main/java/com/aact/eAWB/EAwbApplication.java
@@ -0,0 +1,13 @@
+package com.aact.eAWB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAwbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAwbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aact/eAWB/EAwbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aact.eAWB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAwbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
