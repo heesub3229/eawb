1ef1ee0571ca996d5d4da384a0dc3f9c8d5f3710
HYEYEONG <HYEYEONG@LAPTOP-hyhwang>
2024-01-19 16:06:51 +0900

    20240119entity

---
+++ src/main/java/com/aact/eAWB/Entity/TcmObjectLanguage.java
@@ -0,0 +1,49 @@
+package com.aact.eAWB.Entity;
+
+import java.math.BigInteger;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "TCM_OBJECT_LANGUAGE")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TcmObjectLanguage {
+
+	@Id
+	@Column(name="OBJECT_SID")
+	private BigInteger objectSid;
+	@Column(name="LANGUAGE_CODE")
+	private String languageCode;
+	@Column(name="OBJECT_NAME")
+	private String onjectName;
+	@Column(name="REMARKS")
+	private String remarks;
+	@Column(name="USABLE_FLAG")
+	private String usableFlag;
+	@Column(name="CREATED_USER_ID")
+	private String createdUserID;
+	@Column(name="CREATED_PROGRAM_ID")
+	private String createdProgramID;
+	@Column(name="CREATED_TIME")
+	private String createdTime;
+	@Column(name="UPDATED_USER_ID")
+	private String updatedUserId;
+	@Column(name="UPDATED_PROGRAM_ID")
+	private String updatedProgramId;
+	@Column(name="UPDATED_TIME")
+	private String updatedTime;
+	@Column(name="PROGRESS_GUID")
+	private String progressGuid;
+	@Column(name="PROCESS_GUID")
+	private String processGuid;
+}
