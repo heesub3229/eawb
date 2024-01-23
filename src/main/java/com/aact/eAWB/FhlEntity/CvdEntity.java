fe6066f4f7afe154034947c7d5dd97fbd5234116
HYEYEONG <HYEYEONG@LAPTOP-hyhwang>
2024-01-23 17:52:51 +0900

    20240123CvdEntity

---
+++ src/main/java/com/aact/eAWB/FhlEntity/CvdEntity.java
@@ -0,0 +1,46 @@
+package com.aact.eAWB.FhlEntity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "TIF_FHL_CVD")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CvdEntity {
+	
+	@EmbeddedId
+	private FhlEntityId id;
+	@Column(name="GROUP_NO")
+	private String groupNo;
+	@Column(name="LINE_ID")
+	private String lineId;
+	@Column(name="CURRENCY_CODE")
+	private String currencyCode;
+	@Column(name="PC_WGT_VAL")
+	private String pcWgtVal;
+	@Column(name="PC_OTH_CHG")
+	private String pcOthChg;
+	@Column(name="CARRIAGE_VALUE")
+	private String carriageValue;
+	@Column(name="CUSTOMS_VALUE")
+	private String customsValue;
+	@Column(name="INSURANCE_VALUE")
+	private String insuranceValue;
+	@Column(name="EDI_FLAG")
+	private String ediFlag;
+	@Column(name="CREATED_TIME")
+	private Date createdTime;
+
+}
